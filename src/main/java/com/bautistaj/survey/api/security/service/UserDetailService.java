26147307b19ffee922a91423b99f838ac5248099
Jose Luis Campos Bautista <bautistaj20@outlook.com>
2020-11-29 17:07:02 -0600

    Change heroku configuration

--- src/main/java/com/bautistaj/survey/api/security/service/UserDetailService.java
+++ src/main/java/com/bautistaj/survey/api/security/service/UserDetailService.java
@@ -2,6 +2,8 @@ package com.bautistaj.survey.api.security.service;
 
 import java.util.ArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,12 +15,16 @@ import com.bautistaj.survey.api.repository.IUserRepository;
 
 @Service
 public class UserDetailService implements UserDetailsService{
+	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+	
 	@Autowired
 	private IUserRepository userRepository;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		com.bautistaj.survey.api.model.User user = this.userRepository.findByUsernameAndActive(username, true);
+		LOGGER.debug("Username "+ user.getUsername());
+		LOGGER.debug("Password "+ user.getPassword());
 		return new User(user.getUsername(), user.getPassword(), new ArrayList<>());
 	}
 
